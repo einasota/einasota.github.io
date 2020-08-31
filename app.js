031bfa7e5daba2a7cc9d4d8b49375fb3fd4e2e01
eiNasota <jhonysouza017@gmail.com>
2020-08-30 22:08:09 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+function converter() {
+    const binaryvalue = document.getElementById('b-input').value;
+    if (binaryvalue === '') {
+        return alert('Por favor insira um numero binário');
+    }
+    ;
+    binaryvalue.split('').map((num => {
+        if (num !== '0' && num !== '1') {
+            return alert('Por favor insira um numero binário');
+        }
+    }));
+    const decimal = parseInt(binaryvalue, 2);
+    document.getElementById('d-entry').value = decimal;
+    return true;
+}
